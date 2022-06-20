98b408535a8168451dc837ecc13ae577e2eb80ce
Bastián González-Bustamante <bastian.gonzalezbustamante@politics.ox.ac.uk>
2022-06-20 18:06:15 +0100

    Create script.R

---
+++ code/script.R
@@ -0,0 +1,16 @@
+## The Hitchhiker’s Code
+## Bastián González-Bustamante Script
+## June 2022
+
+library("tableone")
+library("DALEX")
+library("ggplot2")
+library("partykit")
+library("mlr3")
+library("mlr3learners")
+library("ranger")
+library("mlr3tuning")
+library("paradox")
+
+set.seed(1313)
+
